8053b8f6765156f55c1e5e0ec76ec16e1e321b53
zhaohangyi <1806472041@qq.com>
2024-01-30 10:26:06 +0800

    组件的局部注册和全局注册

--- demo1/src/App.vue
+++ demo1/src/App.vue
@@ -1,28 +1,33 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="App">
+    <!-- 头部组件 -->
+    <HmHeader></HmHeader>
+    <!-- 主体组件 -->
+    <HmMain></HmMain>
+    <!-- 底部组件 -->
+    <HmFooter></HmFooter>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import HmFooter from './components/HmFooter.vue';
+import HmHeader from './components/HmHeader.vue';
+import HmMain from './components/HmMain.vue';
 export default {
-  name: 'App',
   components: {
-    HelloWorld
+    HmHeader,
+    HmFooter,
+    HmMain,
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.App {
+  width: 600px;
+  height: 700px;
+  background-color: #87ceeb;
+  margin: 0 auto;
+  padding: 20px;
 }
 </style>
--- demo1/src/main.js
+++ demo1/src/main.js
@@ -1,7 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
-
+import HmButton from './components/HmButton'
 Vue.config.productionTip = false
+
+// 进行全局注册
+Vue.component("HmButton", HmButton)
 
 new Vue({
   render: h => h(App),
