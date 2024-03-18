9e5934f3723999b46a24d5c968f64fc9e7bd7946
zhaohangyi <1806472041@qq.com>
2024-03-18 17:07:24 +0800

    小黑记事本-组件版

--- demo1/readme.md
+++ demo1/readme.md
@@ -1,10 +1,8 @@
-# 组件通信
-
-
+# 组件通信  
 父子关系：props 和 $emit
-1、父组件通过 props 将数据传递给子组件
-（1） 给组件添加标签，添加属性的方式，传值
-（2）通过props进行接收
+1、父组件通过 props 将数据传递给子组件 
+（1）父组件中给组件添加标签，添加属性的方式，传值
+（2）子组件中通过props进行接收
 （3）渲染使用
 2、子组件利用 $emit通知父组件修改更新
 （1）使用$emit触发事件，给父组件发送消息通知 this.$emit("方法名",value)
@@ -26,3 +24,35 @@
 （1）创建.vue组件（单文件组件）
 
 （2）main.js内导入，并且全局注册 Vue.component(组件名，组件对象)
+
+# scoped样式冲突
+1、 默认的style样式，会作用于全局样式
+2、 加上了scoped属性的style样式，只会作用于当前组件 局部样式  
+```
+<style scoped>
+```
+原理： （1）给当前组件模版的所有元素，添加上一个自定义属性的 data-v-hash值，区分不同的组件
+（2）css选择器后面，被自动处理，添加上了属性选择器。
+
+推荐加上 scoped
+
+# props校验
+```vue
+## 基础写法 只校验类型
+props :{
+    校验的属性名：类型 // Number，String，Boolean 
+}
+## 完整写法
+props : {
+    校验的属性名:{
+        type:类型， // Number，String，Boolean
+        required: true, //是否必填
+        default:  默认值,
+        validator(value){
+            // 自定义校验逻辑
+            return 是否通过校验
+        }
+    }
+}
+
+```
--- demo1/src/App.vue
+++ demo1/src/App.vue
@@ -1,40 +1,47 @@
 <template>
-  <div class="App">
-    <!-- 快捷链接 -->
-    <XtxShortCut></XtxShortCut>
-    <!-- 顶部导航 -->
-    <XtxHeaderNav></XtxHeaderNav>
-    <!-- 轮播区域 -->
-    <XtxBanner></XtxBanner>
-    <!-- 新鲜好物 -->
-    <XtxNewGoods></XtxNewGoods>
-    <!-- 热门品牌 -->
-    <XtxHotBrand></XtxHotBrand>
-    <!-- 最新专题 -->
-    <XtxTopic></XtxTopic>
-    <!-- 版权底部 -->
-    <XtxFooter></XtxFooter>
-  </div>
+  <!-- 主体区域 -->
+  <section id="app">
+    <TodoHeader @addNewTodo="addNewTodo"></TodoHeader>
+    <TodoMain :list="list" @del="del"></TodoMain>
+    <TodoFooter :list="list" @clean="clean"></TodoFooter>
+  </section>
 </template>
 
 <script>
-import XtxShortCut from "./components/XtxShortCut.vue"
-import XtxHeaderNav from "./components/XtxHeaderNav.vue"
-import XtxBanner from "./components/XtxBanner.vue"
-import XtxNewGoods from "./components/XtxNewGoods.vue"
-import XtxHotBrand from "./components/XtxHotBrand.vue"
-import XtxTopic from "./components/XtxTopic.vue"
-import XtxFooter from "./components/XtxFooter.vue"
+import TodoHeader from './components/TodoHeader.vue'
+import TodoFooter from './components/TodoFooter.vue'
+import TodoMain from './components/TodoMain.vue'
+
 
 export default {
+  // 1. 提供数据 -》 提供在公共的父组件 App.vue
+  // 2. 通过父传子， 将数据传递给 TodoMain
+  // 3. 利用 v-for渲染
+  data() {
+    return {
+      list: [
+        { id: 1, name: '打篮球' },
+        { id: 2, name: '看电影' },
+        { id: 3, name: '逛街' }
+      ],
+    }
+  },
   components: {
-    XtxShortCut,
-    XtxHeaderNav,
-    XtxBanner,
-    XtxNewGoods,
-    XtxHotBrand,
-    XtxTopic,
-    XtxFooter,
+    TodoFooter,
+    TodoHeader,
+    TodoMain,
+  },
+  methods: {
+    addNewTodo(newValue) {
+      this.list.unshift({ id: +new Date(), name: newValue })
+      console.log(newValue)
+    },
+    del(id) {
+      this.list = this.list.filter(item => item.id !== id)
+    },
+    clean() {
+      this.list = []
+    }
   }
 }
 </script>
