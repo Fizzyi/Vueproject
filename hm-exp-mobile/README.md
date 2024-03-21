5b5e14f427f75d8d5c4fd38d64074330b5f4b106
zhaohangyi <1806472041@qq.com>
2024-03-21 15:45:21 +0800

    vuex-demo

---
+++ hm-exp-mobile/README.md
@@ -0,0 +1,14 @@
+# ESLint 自动修正代码规范错误
+
+```
+"editor.codeActionsOnSave": {
+        "source.fixAll": true
+    },
+    "editor.formatOnSave": false
+```
+
+# Vuex
+
+vuex是一个vue的状态管理工具，这里的状态指的就是数据，帮助我们管理vue通用的数据
+场景（1）某个状态在很多个组件来使用（个人信息） （2） 多个组件共同维护一份数据（购物车）
+
---
+++ vuex-demo/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <h1>根组件</h1>
+    <input type="text">
+    <SonOne></SonOne>
+    <hr>
+    <SonTwo></SonTwo>
+  </div>
+</template>
+
+<script>
+import SonOne from './components/ SonOne.vue';
+import SonTwo from './components/SonTwo.vue';
+
+export default {
+  name: 'App',
+  components: {
+    SonOne,
+    SonTwo
+  }
+}
+</script>
+
+<style>
+#app {
+  width: 600px;
+  margin: 20px auto;
+  border: 3px solid #ccc;
+  border-radius: 3px;
+  padding: 10px;
+}
+</style>
